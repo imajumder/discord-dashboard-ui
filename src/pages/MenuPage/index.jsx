420861b758302c0f8c8b3bf7fe3488214db1ef21
Domnator939131 <75163257+Domnator939131@users.noreply.github.com>
2021-02-19 20:47:34 +0400

    Update index.jsx

--- src/pages/MenuPage/index.jsx
+++ src/pages/MenuPage/index.jsx
@@ -1,33 +1,10 @@
-import React from 'react';
-import { MenuWrapper } from '../../components';
-
-import {getUserDetails, getGuilds} from '../../utils/api'
-
 export function MenuPage() {
-    const [ setUser] = React.useState(null);
-    const [loading, setLoading] = React.useState(true);
-    const [guilds, setGuilds] = React.useState([]);
 
-    React.useEffect(() => {
-        getUserDetails()
-        .then(({data}) => {
-            setUser(data);
-            setLoading(false);
-            return getGuilds()
-        }).then(({data}) => {
-            console.log(data)
-            setGuilds(data)
-        })
-        .catch((err) => {
-            setLoading(false);
-        });
-    }, [])
 
     return (
-        !loading && (
+         (
             <div>
                 <h1>Home Page</h1>
-                <MenuWrapper guilds={guilds}/>
             </div>
         )
     )
